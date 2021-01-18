51020357bfa1ce73ff132a75d81167f3312daad9
eamarasco <eamarasc@ucalgary.ca>
2021-01-18 03:50:23 -0700

    Removed square brackets for exact pattern match

--- Lesson05_JavaStrings/08_FindMatches/JavaStrings.java
+++ Lesson05_JavaStrings/08_FindMatches/JavaStrings.java
@@ -4,7 +4,7 @@ public class JavaStrings {
 
     public static void main(String[] args) {
         
-        Pattern myPattern = Pattern.compile("[Horses]", Pattern.CASE_INSENSITIVE);
+        Pattern myPattern = Pattern.compile("Horses", Pattern.CASE_INSENSITIVE);
         Matcher myMatcher = myPattern.matcher("Horses are mammals. Seahorses are not.");
         boolean isMatch = myMatcher.find();
         System.out.println(isMatch);
