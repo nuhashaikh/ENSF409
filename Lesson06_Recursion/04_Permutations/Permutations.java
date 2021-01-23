6f69ca25a8e820d811065919bc7e614537985791
Ann Barcomb <ann.barcomb@ucalgary.ca>
2021-01-23 15:16:49 -0700

    Added documentation, and handling for length and requests of 0.

--- Lesson06_Recursion/04_Permutations/Permutations.java
+++ Lesson06_Recursion/04_Permutations/Permutations.java
@@ -1,6 +1,6 @@
 public class Permutations {
 	public static int permutations(int length, int select) {
-                if (select < 0) {
+                if (select < 0 || length < 0) {
                         throw new IllegalArgumentException(
                           "Permutations is not defined for negative numbers");
                 }
@@ -10,10 +10,29 @@ public class Permutations {
                           "Selection cannot exceed array length");
 		}
 
+		// Base case
 		if (select == 0) {
-			return 1;
+			return 0;
+
+		// Recursive case
 		} else {
-			return length * permutations(length-1, select-1);
+			// ret could be 0 because of recursion, or because
+			// of an initial call with 0. If it got that way
+			// because of recursion, discard the value by returning
+			// only the current length.  
+
+			// We might have gotten 0 by hitting the base case,
+			// or by receiving 0 as an initial argument. If it
+			// was the base case, we ignore the 0 returned by
+			// using only the length at this level. It will
+			// still be returned as 0 if that was the original
+			// value.
+			int ret = permutations(length-1, select-1);
+			if (ret==0) {
+				return length;
+			} else {
+				return length * ret;
+			}
 		}	
 	}
 
@@ -31,6 +50,9 @@ public class Permutations {
 		System.out.println(result); // Expect 5
 
 		result = permutations(produce.length, 0);
+		System.out.println(result); // Expect 0
+
+		result = permutations(0,0);
 		System.out.println(result); // Expect 0
 
 	}
