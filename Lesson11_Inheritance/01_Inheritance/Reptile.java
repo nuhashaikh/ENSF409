eaa809651871e77c514799f95e6a7dc288172aa8
Ann Barcomb <ann.barcomb@ucalgary.ca>
2021-02-03 18:58:42 -0700

    Simple example of an animal.

---
+++ Lesson11_Inheritance/01_Inheritance/Reptile.java
@@ -0,0 +1,60 @@
+class Animal {
+	private int age;
+
+	public Animal(int age) {
+		this.setAge(age);
+	}
+
+	public Animal() {
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void eat() {
+		System.out.println("Nom nom nom");
+	}
+}
+
+public class Reptile extends Animal {
+	private String scales;
+
+	public Reptile(int age) {
+		super(age);
+	}
+
+	public Reptile(int age, String scales) {
+		super(age);
+		setScales(scales);
+	}
+
+	
+	public String getScales() {
+		return this.scales;
+	}
+
+	public void setScales(String scales) {
+		this.scales = scales;
+	}
+
+
+	public static void  main(String[] args) {
+		Reptile chameleon = new Reptile(2, "multi-hued");
+		Reptile gekko = new Reptile(1);
+
+		System.out.println("Chameleons are " +
+			chameleon.getScales() + " and " +
+			"this one is " + chameleon.getAge() +
+			" years old.");
+
+		System.out.println("This gekko is " +
+			gekko.getAge() + " years old and is" +
+			" enjoying a meal.");
+		gekko.eat();
+	}
+}
