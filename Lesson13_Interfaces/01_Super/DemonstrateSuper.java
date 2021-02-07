1e361dc63d5f0a6f53edae3749cfbaa5424fe4ac
Ann Barcomb <ann.barcomb@ucalgary.ca>
2021-02-06 19:16:17 -0700

    Including examples of super() drawn from both interfaces & intheritance

--- Lesson13_Interfaces/01_Super/DemonstrateSuper.java
+++ Lesson13_Interfaces/01_Super/DemonstrateSuper.java
@@ -1,27 +1,71 @@
-class ChildClass extends ParentClass {
+class ChildClassA implements ParentInterfaceA {
 
-	int x = 409;
-
+	// Demonstrate the use of Interface.super.methodName() - Interface
 	public void display() {
-		System.out.println("Method display called from ChildClass");
-		System.out.println("Subclass x value: " + x); 
-		System.out.println("Super class x value: " + super.x); 
-		super.display(); 
+		System.out.println("Method display called from ChildClassA");
+		ParentInterfaceA.super.display();
 	}
 }
+
+interface ParentInterfaceA {
+	default void display() {
+		System.out.println("Method display called from ParentInterfaceA");
+	}
+}
+
+
+class ChildClassB extends ParentClassB {
+	int x = 1;
+
+	// Demonstrate the use of super() constructor - Inheritance
+	public ChildClassB() {
+		super();
+	}
+
+	public ChildClassB(int x) {
+		super(x);
+	}
+
+	// Demonstrate use of super.methodName() - Inheritance
+	public void display() {
+		System.out.println("Method display called from ChildClassB");
+		System.out.println("ChildClass x value: " + x); 
+		System.out.println("ParentClass x value: " + super.x); 
+		super.display(); 
+	}
+
+
+}
+
+class ParentClassB {
+	int x = 0;
+
+	public ParentClassB() {
+	}
+
+	public ParentClassB(int x) {
+		this.x = x;	
+	}
+
+	public void display() {
+		System.out.println("Method display called from ParentClassB");
+	}
+}
+
 
 public class DemonstrateSuper {
 	public static void main(String args[]) {
-		var childObject = new ChildClass();
-		childObject.display();
-	}
-}
+		var childObjectA = new ChildClassA();
+		childObjectA.display();
+		System.out.println("");
 
+		var childObjectB = new ChildClassB();
+		System.out.println("No value supplied to constructor");
+		childObjectB.display();
+		System.out.println("");
 
-class ParentClass {
-	int x = 1;
-  
-	public void display() {
-		System.out.println("Method display called from ParentClass");
+		childObjectB = new ChildClassB(409);
+		System.out.println("Value supplied to constructor");
+		childObjectB.display();
 	}
 }
