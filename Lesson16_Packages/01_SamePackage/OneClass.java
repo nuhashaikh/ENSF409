efc4387b51667e0f9bbc4d6a906540cc478564f9
Ann Barcomb <ann.barcomb@ucalgary.ca>
2021-02-27 14:21:33 -0700

    Example involving classes in the same package

---
+++ Lesson16_Packages/01_SamePackage/OneClass.java
@@ -0,0 +1,7 @@
+public class OneClass {
+
+	public static void main(String args[]) {
+		TwoClass object = new TwoClass(2);
+		object.output();
+	}
+}
---
+++ Lesson16_Packages/01_SamePackage/TwoClass.java
@@ -0,0 +1,12 @@
+public class TwoClass {
+	private int value;
+
+	public TwoClass(int number) {
+		this.value = number;
+	}
+
+	public void output() {
+		System.out.println("TwoClass was instantiated with " +
+		value);
+	}
+}
