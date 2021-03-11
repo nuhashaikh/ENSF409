8340dbeecaa431a1ded8fbe1521c1f69a0b5855d
Ann Barcomb <ann.barcomb@ucalgary.ca>
2021-03-11 00:32:44 -0700

    Clarification about when to throw the exception

--- Lesson19_IOStream/09_Internationalization/Translator.java
+++ Lesson19_IOStream/09_Internationalization/Translator.java
@@ -33,7 +33,8 @@ package edu.ucalgary.ensf409;
   /* importFromText()
    * Reads in from a the two-letter language code, dash, two-letter region code text 
    * file, in the form of ab-XY.txt, and instantiates a TranslationText object with
-   * the data. It can throw I/O exceptions, as well as a custom ArgFileNotFoundException. 
+   * the data. It can throw I/O exceptions. Throw a custom ArgFileNotFoundException
+   * when the file isn't found. 
    * We expect the .txt file to be in a valid format. The file is expected to be in the same 
    * directory. The files en-US.txt and el-GR.txt are examples of a valid .txt files. They will 
    * always consist of the month names, one per line, followed by the day names, one per line, 
