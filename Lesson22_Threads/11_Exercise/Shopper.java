624bbf288d88bb5774e4bc66ec3c6e71f12ab7d7
Ann Barcomb <ann.barcomb@ucalgary.ca>
2021-03-28 21:20:16 -0600

    Exercise 22.3

---
+++ Lesson22_Threads/11_Exercise/Shopper.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+class Shopper implements Runnable {
+	private Shipping shipping;
+
+	public Shopper(Shipping shipping) {
+		this.shipping = shipping;
+	}
+
+	public void run() {
+		while (true) {
+			String item = null;
+			try {
+				item = shipping.get();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			if (item.equals("NONE"))
+				break;
+			System.out.format("Item picked up by shopper: %s%n", item);
+			try {
+				Thread.sleep(new Random().nextInt(7000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
